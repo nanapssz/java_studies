75bbe71c8dca4e128bdc2a0066d7153b0e66e500
Ana Paula <104795787+nanapssz@users.noreply.github.com>
2026-05-18 21:25:21 -0300

    attttttt

--- AulaTemplateMethod/src/templatemethod3/AutorizadorCirurgia.java
+++ AulaTemplateMethod/src/templatemethod3/AutorizadorCirurgia.java
@@ -4,10 +4,39 @@
  */
 package templatemethod3;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author 02464024236
  */
-public class AutorizadorCirurgia {
+public class AutorizadorCirurgia extends AutorizarServico {
+
+    @Override
+    protected boolean verificarCobertura(SolicitacaoServico solicitacao, List<String> log) {
+        
+        List<String> espSemCobertura = Arrays.asList("FISIOTERAPIA", "CIRURGIA GERAL");
+        
+        boolean permitirCobertura = espSemCobertura.stream().allMatch(log::contains);
+        
+        return permitirCobertura;
+        
+    }
+
+    @Override
+    protected boolean verificarCarencia(SolicitacaoServico solicitacao, List<String> log) {
+        return true;
+    }
+
+    @Override
+    protected boolean verificarLimiteAnual(SolicitacaoServico solicitacao, List<String> log) {
+        return true;
+    }
+
+    @Override
+    protected double calcularValorPrestador(SolicitacaoServico solicitacao) {
+        return 0.0;
+    }
     
 }
